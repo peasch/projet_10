37185ca52cf6c098723f9ab5e77a03e62c2979c3
Pierre-Alain Schaming <peaschaming@gmail.com>
2021-03-31 17:23:34 +0200

    commit mise en place ticket 2, prolongation impossible si date dépassée modification de petites erreurs du commit précédent

---
+++ batch/src/main/java/com/library/batch/Beans/WaitListBean.java
@@ -0,0 +1,64 @@
+package com.library.batch.Beans;
+
+import java.time.LocalDate;
+
+public class WaitListBean {
+
+    private int id;
+    private LocalDate waitListDate;
+    private LocalDate firstReturnDate;
+    private LocalDate contactDate;
+    private UserBean user;
+    private BookBean book;
+
+    public WaitListBean() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getWaitListDate() {
+        return waitListDate;
+    }
+
+    public void setWaitListDate(LocalDate waitListDate) {
+        this.waitListDate = waitListDate;
+    }
+
+    public LocalDate getFirstReturnDate() {
+        return firstReturnDate;
+    }
+
+    public void setFirstReturnDate(LocalDate firstReturnDate) {
+        this.firstReturnDate = firstReturnDate;
+    }
+
+    public LocalDate getContactDate() {
+        return contactDate;
+    }
+
+    public void setContactDate(LocalDate contactDate) {
+        this.contactDate = contactDate;
+    }
+
+    public UserBean getUser() {
+        return user;
+    }
+
+    public void setUser(UserBean user) {
+        this.user = user;
+    }
+
+    public BookBean getBook() {
+        return book;
+    }
+
+    public void setBook(BookBean book) {
+        this.book = book;
+    }
+}
---
+++ client web/src/main/java/com/peasch/webbooks/Beans/WaitListBean.java
@@ -0,0 +1,54 @@
+package com.peasch.webbooks.Beans;
+
+import java.time.LocalDate;
+
+public class WaitListBean {
+    private int id;
+    private LocalDate waitListDate;
+    private LocalDate firstReturnDate;
+    private UserBean user;
+    private BookBean book;
+
+    public WaitListBean() {
+    }
+
+    public LocalDate getWaitListDate() {
+        return waitListDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setWaitListDate(LocalDate waitListDate) {
+        this.waitListDate = waitListDate;
+    }
+
+    public UserBean getUser() {
+        return user;
+    }
+
+    public void setUser(UserBean user) {
+        this.user = user;
+    }
+
+    public BookBean getBook() {
+        return book;
+    }
+
+    public void setBook(BookBean book) {
+        this.book = book;
+    }
+
+    public LocalDate getFirstReturnDate() {
+        return firstReturnDate;
+    }
+
+    public void setFirstReturnDate(LocalDate firstReturnDate) {
+        this.firstReturnDate = firstReturnDate;
+    }
+}
